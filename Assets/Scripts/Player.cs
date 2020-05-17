a3955e904258279b316620ab7af387c37cd9e0d4
白滨 <306558437@qq.com>
2020-05-17 21:58:39 +0800

    05_控制玩家的移动/06_控制图片的移动切换/07_碰撞器的添加，解决抖动问题/08_移动优先级的添加，2D渲染层级问题

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //属性值
+    public float moveSpeed = 3;
+
+    //引用
+    private SpriteRenderer sr;
+    public Sprite[] tankSprite;  //上 右 下 左
+
+    //初始化
+    private void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+
+        
+    }
+
+    //坦克的移动方法
+    private void Move()
+    {
+        float v = Input.GetAxisRaw("Vertical");
+        transform.Translate(Vector3.up * v * moveSpeed * Time.deltaTime, Space.World);
+
+        if (v < 0)
+        {
+            sr.sprite = tankSprite[2];
+        }
+        else if (v > 0)
+        {
+            sr.sprite = tankSprite[0];
+        }
+
+        if (v != 0)
+        {
+            return;
+        }
+
+        float h = Input.GetAxisRaw("Horizontal");
+        transform.Translate(Vector3.right * h * moveSpeed * Time.deltaTime, Space.World);
+        //注：Time.deltaTime（按每秒移动 不加是按没帧移动） Space.World（按世界坐标轴移动）
+
+        if (h < 0)
+        {
+            sr.sprite = tankSprite[3];
+        }
+        else if (h > 0)
+        {
+            sr.sprite = tankSprite[1];
+        }
+    }
+}
