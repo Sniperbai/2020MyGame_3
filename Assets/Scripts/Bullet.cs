b3059766f17147032f623010b2423b2e4bdedc61
白滨 <306558437@qq.com>
2020-05-21 10:06:54 +0800

    13-空气墙的制作，标签的添加

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -20,6 +20,21 @@ public class Bullet : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        //TODO
+        switch (collision.tag)
+        {
+            case "Tank":
+                break;
+            case "Heart":
+                break;
+            case "Enemy":
+                break;
+            case "Wall":
+                break;
+            case "Barrier":
+                break;
+
+            default:
+                break;
+        }
     }
 }
