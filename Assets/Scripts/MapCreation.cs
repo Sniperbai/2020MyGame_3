2ce3b5ccc04493c8301ac2d45be644b630e0bf40
白滨 <306558437@qq.com>
2020-05-26 07:17:28 +0800

    21-初始化地图工具的创建

---
+++ Assets/Scripts/MapCreation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapCreation : MonoBehaviour
+{
+    //用来装饰初始化地图所需要的数组
+    //0.老家 1.墙 2.障碍 3.出生效果 4.河流 5.操 6.空气墙
+    public GameObject[] item;
+
+    private void Awake() 
+    {
+        //实例化老家
+        CreateItem(item[0],new Vector3(0,-8,0),Quaternion.identity);
+        //用墙把老家围起来
+        CreateItem(item[1], new Vector3(-1, -8, 0), Quaternion.identity);
+        CreateItem(item[1], new Vector3(1, -8, 0), Quaternion.identity);
+        for (int i = -1; i < 2; i++) 
+        {
+            CreateItem(item[1],new Vector3(i,-7,0),Quaternion.identity);
+        }
+    }
+
+    private void CreateItem(GameObject createGameObject, Vector3 createPosition, Quaternion createRotation)
+    {
+        GameObject itemGo = Instantiate(createGameObject, createPosition, createRotation);
+        itemGo.transform.SetParent(gameObject.transform);
+    }
+}
