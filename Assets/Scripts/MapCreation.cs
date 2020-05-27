615b537d1d4bc26ee39d88de4614ee9ffedfbf7d
白滨 <306558437@qq.com>
2020-05-27 10:45:57 +0800

    22-编写产生随机位置的方法

--- Assets/Scripts/MapCreation.cs
+++ Assets/Scripts/MapCreation.cs
@@ -8,16 +8,36 @@ public class MapCreation : MonoBehaviour
     //0.老家 1.墙 2.障碍 3.出生效果 4.河流 5.操 6.空气墙
     public GameObject[] item;
 
-    private void Awake() 
+    //已经有东西的位置列表
+    private List<Vector3> itemPositionList = new List<Vector3>();
+
+    private void Awake()
     {
         //实例化老家
-        CreateItem(item[0],new Vector3(0,-8,0),Quaternion.identity);
+        CreateItem(item[0], new Vector3(0, -8, 0), Quaternion.identity);
         //用墙把老家围起来
         CreateItem(item[1], new Vector3(-1, -8, 0), Quaternion.identity);
         CreateItem(item[1], new Vector3(1, -8, 0), Quaternion.identity);
-        for (int i = -1; i < 2; i++) 
+        for (int i = -1; i < 2; i++)
         {
-            CreateItem(item[1],new Vector3(i,-7,0),Quaternion.identity);
+            CreateItem(item[1], new Vector3(i, -7, 0), Quaternion.identity);
+        }
+        //实例化外围墙
+        for (int i = -11; i < 12; i++)      //上
+        {
+            CreateItem(item[6], new Vector3(i, 9, 0), Quaternion.identity);
+        }
+        for (int i = -11; i < 12; i++)      //下
+        {
+            CreateItem(item[6], new Vector3(i, -9, 0), Quaternion.identity);
+        }
+        for (int i = -8; i < 9; i++)      //左
+        {
+            CreateItem(item[6], new Vector3(-11, i, 0), Quaternion.identity);
+        }
+        for (int i = -8; i < 9; i++)      //右
+        {
+            CreateItem(item[6], new Vector3(11, i, 0), Quaternion.identity);
         }
     }
 
@@ -25,5 +45,37 @@ public class MapCreation : MonoBehaviour
     {
         GameObject itemGo = Instantiate(createGameObject, createPosition, createRotation);
         itemGo.transform.SetParent(gameObject.transform);
+
+        itemPositionList.Add(createPosition);
+    }
+
+    //产生随机位置的方法
+    private Vector3 CreateRandomPosition()
+    {
+        //不生成x=-10,10的两列，y=-8，8的两行的位置
+        while (true)
+        {
+            Vector3 createPosition = new Vector3(Random.Range(-9, 10), Random.Range(-8, 8), 0);
+
+            if (!HasThePosition(createPosition))
+            {
+                return createPosition;
+            }
+
+        }
+    }
+
+    //用来判断位置列表中是否有这个位置
+    private bool HasThePosition(Vector3 createPos)
+    {
+        for (int i = 0; i < itemPositionList.Count; i++)
+        {
+            if (createPos == itemPositionList[i])
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
